9ef428d4ece47f93b6709a83699f3b3295cbd3fc
scam <scamii@yandex.ru>
2024-01-01 20:09:27 +0500

    Не большие изменения

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -7,7 +7,8 @@ async def get_start(message: Message, bot: Bot):
     await message.answer(f'<s>Пртвет {message.from_user.first_name}. Рад тебя видеть!</s>')
     await message.reply(f'<tg-spoiler>Пртвет {message.from_user.first_name}. Рад тебя видеть!</tg-spoiler>')
 
+
 async def get_photo(message: Message, bot: Bot):
     await message.answer(f'Отлично. Ты отправил картинку, я сохраню ее себе.')
     file = await bot.get_file(message.photo[-1].file_id)
-    await bot.download_file(file.file_paht, 'photo.jpg')
+    await bot.download_file(file.file_paht, 'photo.png')
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message, ContentType
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.types import Message
 from core.handlers.basic import get_start, get_photo
 import asyncio
 import logging
 from core.settings import settings
-from aiogram.filters import ContentTypesFilter, Command, CommandStart
+from aiogram.filters import Command, CommandStart
 
 
 async def start_bot(bot: Bot):
@@ -25,9 +25,9 @@ async def start():
     dp = Dispatcher()
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
-    dp.message.register(get_photo, ContentTypesFilter(content_types=[ContentType.PHOTO]))
-    dp.message.register(get_start, Command(commands=['start', 'run']))
-    #dp.message.register(get_start, CommandStart)
+    dp.message.register(get_photo, F.photo)
+    #dp.message.register(get_start, Command(commands=['start', 'run']))
+    dp.message.register(get_start, CommandStart)
 
     try:
         await dp.start_polling(bot)
